02b0b435f2eb9432be6c2d980d8478b72a27eb3c
Gazenfar Syed <syedg1@mcmaster.ca>
2021-04-28 02:15:49 -0400

    Heroku port compatibility

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // Required packages
 var app = require('express')();
 var http = require('http').Server(app);
-
+var port = process.env.PORT || 3000;
 // Sends back whatever files are requested (wildcard)
 app.get(/^(.+)$/, function(req,res){
   res.sendFile(__dirname + req.params[0]);
 });
 
 // Start the server
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
 
